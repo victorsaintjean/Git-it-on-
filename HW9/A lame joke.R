90bf38df35cf2dd47206ba8c05839385e2edb1d6
victorsaintjean <victor.saintjean@orange.fr>
2016-11-28 14:36:43 +0100

    Add files via upload

---
+++ HW9/A lame joke.R
@@ -0,0 +1,11 @@
+#A Joke function
+#This function outputs a lame joke, and that's all
+
+setwd(paste(dir, name, sep=""))
+document()
+
+print("Did you hear about the mathematician who's afraid of negative numbers?")
+Sys.sleep(1)
+print("Wait for it")
+Sys.sleep(3)
+print("he'll stop at nothing to avoid them")
